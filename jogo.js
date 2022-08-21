7d26aab2cb9cb64b440984709be40e68f5b5b2d2
Damião Regis de Farias Barbosa <43006221+damiaoregis@users.noreply.github.com>
2022-08-21 03:03:21 -0300

    version 1.2

--- jogo.js
+++ jogo.js
@@ -1,6 +1,7 @@
 var altura = 0
 var largura = 0
 var vidas = 1
+var tempo = 10
 function ajusteTamanhoPalcoJogo(){
     altura = window.innerHeight
     largura = window.innerWidth
@@ -9,6 +10,17 @@ function ajusteTamanhoPalcoJogo(){
 }
 
 ajusteTamanhoPalcoJogo()
+
+var cronometro = setInterval(function(){
+    tempo--
+    if(tempo < 0){
+       clearInterval(cronometro)
+       clearInterval(criaMosquito)
+       window.location.href = "vitoria.html"
+    }else{
+       document.getElementById("cronometro").innerHTML = tempo
+    }
+},1000)
 
 function posicaoRandomica(){
 
